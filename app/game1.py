46f8b835db48f4dcaa0960cf178298f4db66ebf8
sayanamashanlo <mashanlo.shamil@icloud.com>
2024-11-28 18:22:38 +0600

    first commit

---
+++ app/game1.py
@@ -0,0 +1,78 @@
+# import pygame
+# import time
+# import random
+#
+# pygame.init()
+# white = (255, 255, 255)
+# yellow = (255, 255, 255)
+# black = (0, 0, 0)
+# red = (213, 50, 80)
+# green = (0, 255, 0)
+# blue = (50, 153, 213)
+# dis_width = 800
+# dis_height = 600
+# dis = pygame.display.set_mode((dis_width, dis_height))
+# pygame.display.set_caption('Змейка')
+# clock = pygame.time.Clock()
+# snake_block = 10
+# snake_speed = 15
+# font_style = pygame.font.SysFont("", 25)
+# score_font = pygame.font.SysFont("", 35)
+#
+# def Your_score(score):
+#     value = score_font.render("Ваш счет:", + str(score), True, yellow)
+#     dis.blit(value, [0, 0])
+#
+# def our_snake(snake_block, snake_list):
+#     for x in snake_list:
+#         pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
+#
+# def message(msg, color):
+#     mesg = font_style.render(msg, True, color)
+#     dis.blit(mesg, [dis_width / 6, dis_height / 3])
+#
+# def gameLoop():
+#     game_over = False
+#     game_close = False
+#     x1 = dis_width / 2
+#     x2 = dis_height / 2
+#     x1_change = 0
+#     x2_change = 0
+#     snake_list = []
+#     Lenght_of_snake = 1
+#     foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+#     foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+#     while not game_over:
+#         dis.fill(blue)
+#         message("Вы проиграли! Нажмите Q для выхода или С для повторной игры", red)
+#         Your_score(Lenght_of_snake - 1 )
+#         pygame.display.update()
+#         for event in pygame.event.get():
+#             if event.type == pygame.KEYDOWN:
+#                 if event.key == pygame.K_q:
+#                     game_over = True
+#                     game_close = False
+#                 if event.key == pygame.K_c:
+#                     gameLoop()
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             game_over = True
+#         if event.type == pygame.QUIT:
+#             game_over = True
+#         if event.type == pygame.KEYDOWN:
+#             if event.key == pygame.K_LEFT:
+#                 x1_change = -snake_block
+#                 y1_change = 0
+#             elif event.key == pygame.K_RIGHT:
+#                 x1_change = snake_block
+#                 y1_change = 0
+#             elif event.key == pygame.K_UP:
+#                 y1_change = -snake_block
+#                 x1_change = 0
+#             elif event.key == pygame.K_DOWN:
+#                 y1_change = snake_block
+#                 x1_change = 0
+#         if x1 > dis_width or x1 < 0 or y1 > dis_height or y1 < 0:
+#
+
+
