63d5e78581d579af7fb021675791167057588b68
sayanamashanlo <mashanlo.shamil@icloud.com>
2024-12-03 01:15:14 +0600

    fix

--- app/game3.py
+++ app/game3.py
@@ -2,8 +2,8 @@ from rembg import remove
 from PIL import Image
 
 def remove_background(input_path, output_path):
-    input_path = Image.open(input_path)
-    output_path = remove(input_path)
+    input_image = Image.open(input_path)
+    output_image = remove(input_image)
     output_image.save(output_path)
 
 if __name__ == "__main__":
